788ea2f7767c19164e579b3d22282f6b2a5dd749
GYUHA_HWANG <gyuha0310@gmail.com>
2020-12-02 23:36:42 +0900

    routes/comments.js 작성

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/comments", commentsRouter);
+
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
